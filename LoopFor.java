050c8d6295659d42916d34a3add990ea7272c904
Andreas Mora75 <70494818+andreasmora@users.noreply.github.com>
2022-05-19 17:32:43 -0300

    Add files via upload

---
+++ LoopFor.java
@@ -0,0 +1,27 @@
+public class LoopFor {
+    public static void main(String[] args) {
+
+        // FOR COM UMA VARIAVEL
+
+       /* System.out.println("Com ordem crescente!");
+
+        for (int i = 0; i <= 5; i++){
+            System.out.println("i tem valor: " + i);
+
+        }
+            System.out.println("Agora com ordem decrescente!");
+
+        for (int i = 5; i >= 0; i--){
+            System.out.println("i tem valor: " + i);
+
+        } */
+
+      // FOR COM MAIS DE UMA VARIAVEL
+
+        System.out.println("comando ( FOR ) com mais de uma variavel!!");
+
+        for (int i = 0, j = 10; i < j; i ++, j --) {
+            System.out.println("i tem valor: " + i + " ; j tem valor: " + j);
+        }
+    }
+}
--- LoopWhile.java
+++ LoopWhile.java
@@ -7,19 +7,19 @@ public class LoopWhile {
         int i = 1; // count ou cont.
         int max = 10;
 
-        System.out.println("Contando ate: " + max);
+            System.out.println("Contando ate: " + max);
 
         while (i <= max){
             System.out.println("Valor de i: " + i);
             i++; // i = i + 1 / i += 1;
 
         }
-        System.out.println("(Proximo numero apos o limite)" + i); // valor de 21
+            System.out.println("(Proximo numero apos o limite)" + i); // valor de 21
 
         do {
             i++;
             System.out.println("Valor de i: " + i);
         } while (i < 15);
-        System.out.println(i);
+            System.out.println(i);
     }
 }
